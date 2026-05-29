ec3c36376841b00d623a7820f4ce8fce3acf536e
Flower Ornament <31774+flowerornament@users.noreply.github.com>
2026-05-28 20:57:26 -0700

    fix: vendor cargo deps via cargo

--- nix/package.nix
+++ nix/package.nix
@@ -3,8 +3,33 @@ pkgs.rustPlatform.buildRustPackage {
   pname = "nx";
   inherit version src;
 
-  cargoLock = {
-    lockFile = src + /Cargo.lock;
+  cargoDeps = pkgs.stdenvNoCC.mkDerivation {
+    pname = "nx-cargo-vendor";
+    inherit version src;
+
+    nativeBuildInputs = [ pkgs.cargo pkgs.cacert ];
+
+    buildPhase = ''
+      runHook preBuild
+
+      export HOME="$TMPDIR"
+      export CARGO_HOME="$TMPDIR/cargo-home"
+      export CARGO_HTTP_CAINFO="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+      export CARGO_HTTP_USER_AGENT="cargo/${pkgs.cargo.version} nx-rs-nix-vendor"
+
+      cargo vendor --locked --versioned-dirs "$out"
+      cp Cargo.lock "$out/Cargo.lock"
+
+      runHook postBuild
+    '';
+
+    dontConfigure = true;
+    dontInstall = true;
+    dontFixup = true;
+
+    outputHash = "sha256-OyLHuOHvoeZIBTG7mYmzzsa48WaHNaw3SpJDft0dba8=";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
   };
 
   nativeCheckInputs = [ pkgs.git pkgs.which ];
